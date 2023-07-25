4d0e77f57bfe91fde8ffe14d9fc793efc567d24e
jiseonn97 <jiseonn97@naver.com>
2023-07-25 18:18:26 +0900

    230725

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -20,3 +20,71 @@ $(document).ready(function () {
         },
     });
 });
+
+// $(document).ready(function () {
+//     var tl = gsap.timeline({
+//         scrollTrigger: {
+//             trigger: ".section-history",
+//             start: "top center",
+//             pin: true,
+//             end: "'bottom center",
+//             scrub: 1,
+//             invalidateOnRefresh: true,
+//         },
+//         defaults: {
+//             ease: "power2.out",
+//         },
+//     });
+// });
+
+// var tl = gsap.timeline();
+
+// tl.set(".sec-contents", {
+//     width: "768",
+//     height: "559",
+// });
+
+// tl.to(".sec-contents", {
+//     scale: 1.5,
+//     width: "100%",
+//     height: "100vh",
+// });
+
+// ScrollTrigger.matchMedia({
+
+// "(min-width:821px)" : function(){
+
+//     var bS02Timeline = gsap.timeline({
+//         scrollTrigger : {
+//             trigger : "._b-s02",
+//             start : "top top",
+//             pin : true,
+//             end : "150%",
+//             scrub : 1,
+//             invalidateOnRefresh : true
+//         },
+//         defaults : {
+//             ease: "power2.out"
+//         }
+//     });
+//         bS02Timeline
+//             .set("._b-s02 .txtImg .img",{
+//                 width: "640",
+//                 height: "323"
+//             })
+//             .set('._b-s02 .txtImg h2:nth-of-type(1)',{
+//                 x : "-20.833vw"
+//             })
+//             .set('._b-s02 .txtImg h2:nth-of-type(2)',{
+//                 x: "20.833vw"
+//             })
+//             .to('._b-s02 .txtImg .img',{
+//                 width : "100vw",
+//                 height : "100vh",
+//             },"my")
+//             .to('._b-s02 .txtImg h2:nth-of-type(1)',{
+//                 x : 0
+//             },"my")
+//             .to('._b-s02 .txtImg h2:nth-of-type(2)',{
+//                 x: 0
+//             },"my"
