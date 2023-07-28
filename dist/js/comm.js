493e2d192a9370f41f237a609c74efa768d9ea6b
jiseonn97 <jiseonn97@naver.com>
2023-07-28 17:44:52 +0900

    장바구니, 메인

--- dist/js/comm.js
+++ dist/js/comm.js
@@ -1,11 +1,8 @@
 $(document).ready(function () {
     // Assign some jquery elements we'll need
     var $swiper = $(".swiper-container");
-    var $bottomSlide = null; // Slide whose content gets 'extracted' and placed
-    // into a fixed position for animation purposes
-    var $bottomSlideContent = null; // Slide content that gets passed between the
-    // panning slide stack and the position 'behind'
-    // the stack, needed for correct animation style
+    var $bottomSlide = null;
+    var $bottomSlideContent = null;
 
     var mySwiper = new Swiper(".swiper-container", {
         spaceBetween: 30,
@@ -21,41 +18,11 @@ $(document).ready(function () {
     });
 });
 
-// ScrollTrigger.matchMedia({
+//장바구니 스와이퍼
 
-// "(min-width:821px)" : function()
-
-//     var bS02Timeline = gsap.timeline({
-//         scrollTrigger : {
-//             trigger : "._b-s02",
-//             start : "top top",
-//             pin : true,
-//             end : "150%",
-//             scrub : 1,
-//             invalidateOnRefresh : true
-//         },
-//         defaults : {
-//             ease: "power2.out"
-//         }
-//     });
-//         bS02Timeline
-//             .set("._b-s02 .txtImg .img",{
-//                 width: "640",
-//                 height: "323"
-//             })
-//             .set('._b-s02 .txtImg h2:nth-of-type(1)',{
-//                 x : "-20.833vw"
-//             })
-//             .set('._b-s02 .txtImg h2:nth-of-type(2)',{
-//                 x: "20.833vw"
-//             })
-//             .to('._b-s02 .txtImg .img',{
-//                 width : "100vw",
-//                 height : "100vh",
-//             },"my")
-//             .to('._b-s02 .txtImg h2:nth-of-type(1)',{
-//                 x : 0
-//             },"my")
-//             .to('._b-s02 .txtImg h2:nth-of-type(2)',{
-//                 x: 0
-//             },"my"
+var swiper2 = new Swiper(".swiper-container2", {
+    pagination: ".swiper-pagination",
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+});
